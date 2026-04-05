2dad6d5cc3f943702dbbd448ac676480798188fe
defoltusrnm <defoltusrnm@gmail.com>
2026-04-05 19:06:59 +0300

    fix pub scripts

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-cd ./src/lib/UnionStruct.Core/bin/Release || exit 1
-
-dotnet nuget push ./UnionStruct.0.0.2.nupkg \
-    --api-key "$nuget_api_key" \
-    --source https://api.nuget.org/v3/index.json
+cd .\src\lib\UnionStruct.Core\bin\Release
+dotnet nuget push .\UnionStruct.0.0.6.nupkg --api-key $Env:nuget_api_key --source https://api.nuget.org/v3/index.json
